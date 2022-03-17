09b3eba3bda7ae92cbc762bdec2f43702212af74
Daria <jadarya@mail.ru>
2022-03-17 20:24:15 +0900

    rename to back part

---
+++ crypto-exchanger-back/src/app.module.ts
@@ -0,0 +1,45 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import * as Joi from 'joi';
+import { CommonModule } from './common/common.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod', 'test'),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: process.env.NODE_ENV !== 'prod',
+      logging: process.env.NODE_ENV === 'dev',
+      entities: [],
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      installSubscriptionHandlers: true,
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    CommonModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
