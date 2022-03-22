2b809a90ae0fcc7d90f298f6a5f83d5da341ff99
Daria <jadarya@mail.ru>
2022-03-23 03:26:01 +0900

    add dtos and jwsService

--- crypto-exchanger-back/src/users/users.service.ts
+++ crypto-exchanger-back/src/users/users.service.ts
@@ -5,13 +5,16 @@ import { User } from './entities/user.entity';
 import { Verification } from './entities/verification.entity';
 //import { JwtService } from '../jwt/jwt.service';
 import { CreateAccountInput } from './dtos/create-account.dto';
+import { LoginInput } from './dtos/login.dto';
+import { JwtService } from 'src/jwt/jwt.service';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
     @InjectRepository(Verification) private readonly verification: Repository<Verification>,
-  ) //private readonly jwtService: JwtService,
+   private readonly jwtService: JwtService,
+  )
   {}
 
   async createAccount({
@@ -32,5 +35,28 @@ export class UsersService {
     } catch (e) {
       return { ok: false, error: "Coulndn't create account " };
     }
+  }
+
+  async login({ email, password }: LoginInput): Promise<{ ok: boolean; error?: string token?: string }> {
+    try {
+      const user = await this.users.findOne({ email }, { select: ['id', 'password']});
+      if (!user) {
+        return { ok: false, error: 'User not found'};
+      }
+
+      const passwordCorrect = await user.checkPassword(password);
+      if (!passwordCorrect) {
+        return { ok: false, error: 'Wrong password'};
+      }
+      const token = this.jwtService.sign(user.id);
+
+      return { ok: true, token };
+    } catch (error) {
+      return { ok: false, error };
+    }
+  }
+
+  async findById(id: number): Promise<UserProfileOutput> {
+    
   }
 }
