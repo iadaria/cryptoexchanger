1e7171aee42e90aa9db53942fa396863c1b2bc31
iadaria <jadarya@mail.ru>
2023-07-09 07:34:31 +0900

    fix telega

--- apps/micro-telega-back/src/bot/bot.update.ts
+++ apps/micro-telega-back/src/bot/bot.update.ts
@@ -1,68 +1,80 @@
 import { UseFilters, UseGuards, UseInterceptors } from '@nestjs/common';
-import { Command, Ctx, Hears, Help, InjectBot, Message, On, Sender, Start, TELEGRAF_BOT_NAME, Update } from 'nestjs-telegraf';
+import {
+  Command,
+  Ctx,
+  Hears,
+  Help,
+  InjectBot,
+  Message,
+  On,
+  Sender,
+  Start,
+  TELEGRAF_BOT_NAME,
+  Update,
+} from 'nestjs-telegraf';
 import { StoreUserInterceptor } from 'src/bot/interceptors/store-user.interceptor';
 import { Context } from 'src/common/interfaces/context.interface';
 import { Telegraf } from 'telegraf';
 import { UpdateType as TelegrafUpdateType } from 'telegraf/typings/telegram-types';
-import { MENU_MAIN, TelegramBotName } from './bot.constants';
+import { MENU_MAIN } from './bot.constants';
 import { AdminGuard } from 'src/bot/guards/admin.guard';
 import { UpdateType } from 'src/bot/decorators/update-type.decorator';
 import { ReverseTextPipe } from 'src/bot/pipes/reverse-text.pipe';
 import { BotService } from './bot.service';
 import { TelegrafExceptionFilter } from 'src/common/filters/telegraf-exception.filter';
 
-
 @Update()
 //@UseInterceptors(StoreUserInterceptor)
 @UseFilters(TelegrafExceptionFilter)
 export class BotUpdate {
   constructor(
-    @InjectBot() private readonly bot: Telegraf<Context>,
+    @InjectBot(TELEGRAF_BOT_NAME) private readonly bot: Telegraf<Context>,
     private readonly botService: BotService,
   ) {}
 
   @Start()
   async onStart(@Ctx() ctx: Context): Promise<void> {
-    console.log(ctx.scene)
+    console.log(ctx.scene);
     //console.log(ctx.message)
     await ctx.scene.enter(MENU_MAIN);
     //await ctx.replyWithSticker('123123jkbhj6b');
   }
-  
+
   @Help()
   async onHelp(): Promise<string> {
     return 'Send me any text';
   }
-  
+
   @Command('admin')
   @UseGuards(AdminGuard)
   onAdminCommand(): string {
     return 'Welcome judge';
   }
-  
+
   @Hears(['hi', 'hello', 'hey', 'qq'])
   onGreetings(
     @UpdateType() updateType: TelegrafUpdateType,
     @Sender('first_name') firstName: string,
-    ): string {
-  /*     console.log('***')
+  ): string {
+    /*     console.log('***')
       console.log(updateType); */
-      return `Hey hey ${firstName}`;
-    }
-    
+    return `Hey hey ${firstName}`;
+  }
+
   @Command('scene')
   async onSceneCommand(@Ctx() ctx: Context): Promise<void> {
     const me = await this.bot.telegram.getMe();
     //console.log('command scene ' + me.username)
     //console.log(ctx.scene);
     //console.log(this.bot.context.scene);
-      await ctx.scene.enter(MENU_MAIN);
+    await ctx.scene.enter(MENU_MAIN);
   }
 
   @On('text')
-  onMessage(@Message('text', new ReverseTextPipe()) reversedText: string): string {
+  onMessage(
+    @Message('text', new ReverseTextPipe()) reversedText: string,
+  ): string {
     //console.log('text and echo')
     return this.botService.echo(reversedText);
   }
-  
 }
--- apps/micro-telega-back/src/configs/telegram.config.ts
+++ apps/micro-telega-back/src/configs/telegram.config.ts
@@ -1,36 +1,24 @@
-import { TelegrafModuleAsyncOptions, TelegrafModuleOptions } from "nestjs-telegraf";
+import { TELEGRAF_BOT_NAME, TelegrafModuleAsyncOptions } from 'nestjs-telegraf';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-import { BotModule } from "src/bot/bot.module";
-import { sessionMiddleware } from "src/common/middleware/session.middleware";
-import { getTelegramToken } from "./envs/envs.get";
-import { TelegramBotName } from "src/bot/bot.constants";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-
-
-export const telegramConfig = ():TelegrafModuleOptions => {
-  return{
-  botName: 'test',
-  token: "5000566046:AAHU9EuiHr1yWVg6V1-RXKTel6cSOTQZlfs/test",
-  middlewares: [sessionMiddleware],
-  include: [BotModule],
-}};
-
+import { BotModule } from 'src/bot/bot.module';
+import { sessionMiddleware } from 'src/common/middleware/session.middleware';
 
 export const telegramAsyncOptions = (): TelegrafModuleAsyncOptions => ({
   imports: [ConfigModule],
+  botName: TELEGRAF_BOT_NAME,
   useFactory: (config: ConfigService) => {
-    console.log({ config })
+    console.log({ config });
     return {
-      //botName: 'testBot',
       middlewares: [sessionMiddleware],
       include: [BotModule],
       token: config.get<string>('TELEGRAM_TOKEN'),
       options: {
         telegram: {
-          testEnv: config.get<boolean>('TELEGRAM_TEST')
-        }
-      }
+          testEnv: config.get<boolean>('TELEGRAM_TEST'),
+        },
+      },
     };
   },
-  inject: [ConfigService]
+  inject: [ConfigService],
 });
